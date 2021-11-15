c14b6c1a3e44579b57c3ff27df2e226b8cf01e37
Tobias V. Langhoff <tobias@langhoff.no>
2021-11-15 12:46:41 +0100

    Add decoding to separate files to example binary. Closes #1.

--- examples/decart.rs
+++ examples/decart.rs
@@ -1,4 +1,5 @@
-use std::ffi::OsStr;
+use std::fs::File;
+use std::io::prelude::*;
 use std::path::Path;
 
 use atty::Stream;
@@ -22,14 +23,17 @@ pub fn main() {
             .arg(Arg::with_name("print program")
                 .short("p")
                 .long("print-program")
-                .help("Instead of printing the entire JSON payload to stdout, print just the Octo program source code.")
+                .help("instead of printing the entire JSON payload to stdout, print just the Octo program source code.")
             )
-            .arg(Arg::with_name("to files")
-                .short("t")
-                .long("to-files")
+            .arg(Arg::with_name("write files")
+                .short("w")
+                .long("write-files")
                 .takes_value(true)
-                .value_name("output-file")
-                .help("Instead of printing to stdout, create two files with the contents from the Octocart: An .8o file with the program source code, and an .octo.rc file with the runtime options. If you supply a value here, it will be used as the filenames (plus extensions); if not, the filename of the Octocart will be used.")
+                .value_name("output file stem")
+                .help("Instead of printing to stdout, create two files with the contents from the Octocart: \
+                An .8o file with the program source code, and an .octo.rc file with the runtime options. \
+                If you supply a value here, it will be used as the filenames (plus extensions); \
+                if not, the filename of the Octocart will be used.")
                 .min_values(0)
                 .max_values(1)
             )
@@ -42,17 +46,32 @@ pub fn main() {
         )
         .get_matches();
 
-    if let Some(ref matches) = matches.subcommand_matches("decode") {
+    if let Some(matches) = matches.subcommand_matches("decode") {
         let filename = Path::new(matches.value_of("OCTOCART").unwrap());
-        let cart: OctoCart = from_file(filename).unwrap();
+        let cart: OctoCart = from_file(filename).expect("Failed to read Octocart file");
 
-        if matches.is_present("to files") {
+        if matches.is_present("write files") {
             let path = filename.parent().unwrap();
             let stem = filename.file_stem().unwrap();
-            let base =
-                matches
-                    .value_of("output-file")
-                    .unwrap_or(&format!("{}{:?}", path.display(), stem));
+            let default_base = format!("{}/{}", path.display(), stem.to_str().unwrap());
+            let base = matches.value_of("write files").unwrap_or(&default_base);
+            let octo_file_path = format!("{}.{}", base, "8o");
+            let mut octo_file = File::create(Path::new(&octo_file_path))
+                .unwrap_or_else(|_| panic!("Failed to create {}", octo_file_path));
+            octo_file
+                .write_all(cart.program.as_bytes())
+                .unwrap_or_else(|_| panic!("Failed to write program to {}", octo_file_path));
+            let rc_file_path = format!("{}.{}", base, "octo.rc");
+            let mut rc_file = File::create(Path::new(&rc_file_path))
+                .unwrap_or_else(|_| panic!("Failed to create {}", rc_file_path));
+            rc_file
+                .write_all(octopt::Options::to_ini(cart.options).as_bytes())
+                .unwrap_or_else(|_| panic!("Failed to write options to {}", rc_file_path));
+
+            println!(
+                "Wrote files:\nCode:    {}\nOptions: {}",
+                octo_file_path, rc_file_path
+            );
         } else if matches.is_present("print program") {
             if atty::is(Stream::Stdout) {
                 let mut ssb = SyntaxSetBuilder::new();
@@ -65,7 +84,6 @@ pub fn main() {
                     .unwrap(),
                 );
                 let ps = ssb.build();
-
                 let syntax = ps.find_syntax_by_extension("8o").unwrap();
 
                 let mut theme_cursor = std::io::Cursor::new(include_bytes!("Monokai.tmTheme"));
@@ -83,7 +101,12 @@ pub fn main() {
                 println!("{}", &cart.program);
             }
         } else {
-            println!("{}", cart.to_string().to_colored_json_auto().unwrap());
+            println!(
+                "{}",
+                cart.to_string()
+                    .to_colored_json_auto()
+                    .expect("Failed to print Octocart JSON payload")
+            );
         }
     }
 }
